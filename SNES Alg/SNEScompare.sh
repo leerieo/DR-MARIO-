9d01e3ee20295a61074db7d5ec0a43f03eb642c8
Trang Q. Ngo <tqn1@williams.edu>
2020-07-13 10:38:44 +0700

    should compare level 21 and up (bad seeds)

--- SNES Alg/SNEScompare.sh
+++ SNES Alg/SNEScompare.sh
@@ -1,16 +1,19 @@
 #Compare outputs of nightmareci's main.c for the SNES algorithm
-#with SNES-single-player.py for level 10
+#with SNES-single-player.py for level 21
+#starting from level 21, there are "bad seeds" where the virus generator algorithm
+#generates fewer viruses than expected - should check this
 
-
-#NOTE: output of main.c should be in a file named main  (gcc fileName.c -o main)
+#NOTE1: to run shell script, must change permission modes for script
+#NOTE2: output of main.c should be in a file named main
+#gcc fileName.c -o main
 
 for i in in {0..66535}
 do
   seed=$(echo "obase=16; $i" | bc)
   echo "seed: $seed"
 
-  commandA="./main 10 $seed"
-  commandB="python SNES-single-player.py 10 $seed"
+  commandA="./main 21 $seed"
+  commandB="python SNES-single-player.py 21 $seed"
   diff <($commandA) <($commandB)
 
 done
